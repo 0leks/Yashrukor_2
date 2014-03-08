bd3ead3692bc165da1e008fd4df40098cf244cba
Yashwanth Nannapaneni <nyashu@gmail.com>
2014-03-08 14:40:07 -0800

    Added constructors to Thing and Building

--- src/main/Building.java
+++ src/main/Building.java
@@ -1,8 +1,11 @@
 package main;
 
 public class Building extends Thing {
-	public Building()
+	Player myPlayer;
+	public Building(int x, int y)
 	{
-		
+		this.x = x;
+		this.y = y;
 	}
+
 }
--- src/main/Thing.java
+++ src/main/Thing.java
@@ -5,9 +5,16 @@ public class Thing {
 	protected int y;
 	int MAXHEALTH;
 	int hp;
+	private boolean destroyed;
 	public static World myWorld;
 	public Thing()
 	{
-		
+		destroyed = false; 
 	}
+	public Thing(int x, int y)
+	{
+		destroyed = false;
+		this.x = this.y;
+	}
+	
 }
