39c17a13e55cc5f347f1764567205bb523c05b5d
Yashwanth Nannapaneni <nyashu@gmail.com>
2014-03-08 14:27:48 -0800

    Addes stuff to World and Thing

--- src/main/Thing.java
+++ src/main/Thing.java
@@ -1,6 +1,11 @@
 package main;
 
 public class Thing {
+	protected int x;
+	protected int y;
+	int MAXHEALTH;
+	int hp;
+	public static World myWorld;
 	public Thing()
 	{
 		
--- src/main/Unit.java
+++ src/main/Unit.java
@@ -8,46 +8,53 @@ public class Unit extends Thing {
 	public final int CROSSBOW = 5;
 	public final int MEDIC = 6;
 	private boolean isRanged;
-	private int hp;
 	private int damage;
 	
 	private int unitType;
-	public Unit(int unitType)
+	public Unit(int unitType, int x, int y)
 	{	
+		this.x = x;
+		this.y = y;
 		this.unitType = unitType; 
 		if(unitType == WORKER)
 		{
 			hp = 50;
+			MAXHEALTH = 50;
 			damage =0;
 			isRanged = false;
 		}
 		if(unitType == WARRIOR)
 		{
 			hp = 100;
+			MAXHEALTH = 50;
 			damage = 20;
 			isRanged = false;
 		}
 		if(unitType == ARCHER)
 		{
 			hp = 60;
+			MAXHEALTH = 60;
 			damage = 10;
 			isRanged = true;
 		}
 		if(unitType == KNIGHT)
 		{
 			hp = 200;
+			MAXHEALTH = 200;
 			damage = 40;
 			isRanged = false;
 		}
 		if(unitType == CROSSBOW)
 		{
 			hp = 100;
+			MAXHEALTH = 100;
 			damage = 30;
 			isRanged = true;
 		}
 		if(unitType == MEDIC)
 		{
 			hp = 80;
+			MAXHEALTH = 80;
 			damage = 0; //heals 5 for closest ally
 			isRanged = true; 
 			
