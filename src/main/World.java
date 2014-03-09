15086d65464095bfbb530917642f453492de3edb
Ruijie Shao <bobbistef@gmail.com>
2014-03-08 17:50:45 -0800

    added player to building

--- src/main/World.java
+++ src/main/World.java
@@ -12,16 +12,41 @@ import javax.swing.JPanel;
 public class World implements Serializable {
 	ArrayList<Building> allBuildigs = new ArrayList<Building>();
 	ArrayList<Unit> allUnits = new ArrayList<Unit>();
-	Player temp = new Player();
+	Player t4 = new Player();
+	Player t1=new Player();
+	Player t2=new Player();
+	Player t3=new Player();
+	ArrayList<Player> playerlist=new ArrayList<Player>();
 	Toolkit tk = Toolkit.getDefaultToolkit();
 	int xsize=(int) tk.getScreenSize().getWidth();
 	int ysize=(int) tk.getScreenSize().getHeight();
+	int worldx=4800;
+	int worldy=4800;
 	ArrayList<Thing> allThings = new ArrayList<Thing>();
 	TempFrameForTestingOnly asdf;
 	public World() {
 		Thing.myWorld = this;
-		allThings.add(new Unit(1, 50, 50, temp));
-		allThings.add(new Building(150, 50, 1));
+		playerlist.add(t1);
+		playerlist.add(t2);
+		playerlist.add(t3);
+		playerlist.add(t4);
+		if(playerlist.size()>0){
+			allThings.add(new Unit(1, 0, 50, playerlist.get(0)));
+			allThings.add(new Building(50, 50, 1,playerlist.get(0)));
+		}
+		if(playerlist.size()>1){
+			allThings.add(new Unit(1, 4650, 4700, playerlist.get(1)));
+			allThings.add(new Building(4700, 4700, 1,playerlist.get(1)));
+		}
+		if(playerlist.size()>2){
+			allThings.add(new Unit(1, 0, 4700, playerlist.get(2)));
+			allThings.add(new Building(50, 4700, 1,playerlist.get(2)));
+		}
+		if(playerlist.size()>3){
+			allThings.add(new Unit(1, 4700, 0, playerlist.get(3)));
+			allThings.add(new Building(4700, 50, 1,playerlist.get(3)));
+		}
+		
 		allThings.add(new Thing(150, 150));
 		//TempFrameForTestingOnly asdf = new TempFrameForTestingOnly();
 	}
